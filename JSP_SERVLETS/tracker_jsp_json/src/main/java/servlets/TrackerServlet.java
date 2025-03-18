6ef1d2a6c440a6994193bf72c0626a908a7da9c6
Marcos Dessa (UCSal) <marcosdessa.oliveira@ucsal.edu.br>
2025-03-18 16:32:26 -0300

    Tracker (JSP / JSON): fix

--- JSP_SERVLETS/tracker_jsp_json/src/main/java/servlets/TrackerServlet.java
+++ JSP_SERVLETS/tracker_jsp_json/src/main/java/servlets/TrackerServlet.java
@@ -15,6 +15,8 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 
 import dto.RegistroDto;
 
@@ -35,30 +37,41 @@ public class TrackerServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		// obtendo o código da request
-		BufferedReader reader = request.getReader();
-		Gson gson = new Gson();
-		String codigo = gson.fromJson(reader, Msg.class).codigo;
-		
-		// preparando a response
-		ArrayList<RegistroDto> resposta;
-		Rastreio rastreio = BaseDeDadosSingleton.getInstance().procurarPorCodigo(codigo);
-		if (rastreio != null) {
-			resposta = rastreio.getDtos();
-		} else {
-			resposta = new ArrayList<RegistroDto>();
+		try {
+			// obtendo o código da request
+			BufferedReader reader = request.getReader();
+			Gson gson = new Gson();
+			String codigo = gson.fromJson(reader, Msg.class).codigo;
+			
+			// preparando a response
+			ArrayList<RegistroDto> resposta;
+			Rastreio rastreio = BaseDeDadosSingleton.getInstance().procurarPorCodigo(codigo);
+			if (rastreio != null) {
+				resposta = rastreio.getDtos();
+			} else {
+				resposta = new ArrayList<RegistroDto>();
+			}
+			gson = new Gson();
+			String json = gson.toJson(resposta);
+			
+			// configurando http
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			
+			// incluindo o json na response definitivamente
+			PrintWriter saida = response.getWriter();
+			saida.print(json);
+			saida.flush();
+		} catch (JsonSyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonIOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		gson = new Gson();
-		String json = gson.toJson(resposta);
-		
-		// configurando http
-		response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        
-        // incluindo o json na response definitivamente
-        PrintWriter saida = response.getWriter();
-        saida.print(json);
-        saida.flush();
 
 	}
 
